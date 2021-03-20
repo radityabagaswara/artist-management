d033663810c92cb534df20742af1fd618e1b149f
FatK <officialteamxcraft@gmail.com>
2021-03-21 03:00:02 +0700

    added bootstrap

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href='assets/vendor/bootstrap/css/bootstrap.min.css'>
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Test</h1>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src='assets/vendor/bootstrap/js/bootstrap.min.js'></script>
+</body>
+
+</html>
